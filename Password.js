9eb86aeee1159a7ac16d34528559ce5a32259348
SovietKane <82541447+SovietKane@users.noreply.github.com>
2021-04-29 22:31:07 +0200

    v 1.5

---
+++ Password.js
@@ -0,0 +1,9 @@
+function password(){
+    var p = document.getElementById("password")
+    if(p.value == "Asus"){
+        location.href = "https://www.youtube.com/"
+    }
+    else{
+        console.log("OO")
+    }
+}
